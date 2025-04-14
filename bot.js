661d9371f900b9594330617b21debf1fb63d605a
Z A I I K Y <156815062+quangminh1341@users.noreply.github.com>
2025-04-14 19:51:21 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,7 +17,7 @@ const client = new Client({
   ]
 });
 
-let guildId = '747767032186929212';
+let guildId = '1311497270319124520';
 let channelId = '1313481298504978543';
 
 client.once('ready', () => {
